9b5713532bf3df52523023af31c4534b5a476469
Sergei Romanov <l2omanov@ya.ru>
2020-09-24 22:39:16 +0400

    DB created

--- dating_DB.py
+++ dating_DB.py
@@ -2,7 +2,7 @@ import sqlalchemy as sq
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from db_adm_pass import db_pass, db_admin
-import psycopg2
+# import psycopg2
 
 
 Base = declarative_base()
@@ -12,7 +12,7 @@ Session = sessionmaker(bind=engine)
 
 
 class DatingUser(Base):
-    __tablename__ = 'dating_user'
+    __tablename__ = 'DatingUser'
 
     dating_id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.String)
@@ -22,58 +22,36 @@ class DatingUser(Base):
     age_max = sq.Column(sq.Integer)
     city_name = sq.Column(sq.String)
     city_id = sq.Column(sq.Integer)
-    matchingusers = relationship('MatchingUser', secondary='dating_to_matching', back_populates='datinguser')
-    blacklistedusers = relationship('BlacklistedUser', secondary='dating_to_blacklisted', back_populates='datinguser')
+    matchingusers = relationship('MatchingUser', backref='DatingUser')
+    blacklistedusers = relationship('BlacklistedUser', backref='DatingUser')
 
 class MatchingUser(Base):
-    __tablename__ = 'matchinguser'
+    __tablename__ = 'MatchingUser'
 
     matching_id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.String)
     last_name = sq.Column(sq.String)
     age = sq.Column(sq.Integer)
-    dating_id = relationship(DatingUser)
-    photos = relationship('Photos', secondary='matching_to_photos', back_populates='matchinguser')
+    id_dater = sq.Column(sq.Integer,sq.ForeignKey('DatingUser.dating_id'))
+    photos = relationship('Photos', backref='MatchingUser')
 
 class Photos(Base):
-    __tablename__ = 'userphotos'
+    __tablename__ = 'Photos'
 
     photo_id = sq.Column(sq.Integer, primary_key=True)
-    matching_id = relationship(MatchingUser)
+    id_matcher = sq.Column(sq.Integer, sq.ForeignKey('MatchingUser.matching_id'))
     photo_link = sq.Column(sq.String)
     likes_count = sq.Column(sq.Integer)
 
 class BlacklistedUser(Base):
-    __tablename__ = 'blacklisteduser'
+    __tablename__ = 'BlacklistedUser'
 
     blacklisted_id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.String)
     last_name = sq.Column(sq.String)
     age = sq.Column(sq.Integer)
-    dating_id = relationship(DatingUser)
-
-dating_to_matching = sq.Table(
-    'dating_to_matching', Base.metadata,
-    sq.Column('dating_id', sq.Integer, sq.ForeignKey('datinguser.dating_id')),
-    sq.Column('matching_id', sq.Integer, sq.ForeignKey('matchinguser.matching_id')),
-)
-
-matching_to_photos = sq.Table(
-    'matching_to_photos', Base.metadata,
-    sq.Column('matching_id', sq.Integer, sq.ForeignKey('matchinguser.matching_id')),
-    sq.Column('photo_id'), sq.Integer, sq.ForeignKey('photos.photo_id'),
-)
-
-dating_to_blacklisted = sq.Table(
-    'dating_to_matching', Base.metadata,
-    sq.Column('dating_id', sq.Integer, sq.ForeignKey('datinguser.dating_id')),
-    sq.Column('matching_id', sq.Integer, sq.ForeignKey('blacklisteduser.blacklisted_id')),
-)
+    id_dater = sq.Column(sq.Integer,sq.ForeignKey('DatingUser.dating_id'))
 
 
-
-
-
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
+# if __name__ == '__main__':
+#     Base.metadata.create_all(engine)
